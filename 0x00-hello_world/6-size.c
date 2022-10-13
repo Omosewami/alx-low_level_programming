d1e2e8f5f48536ead1e472c9beae0a71a7a14d20
Omosewami <raimothassan10@gmail.com>
2022-10-13 13:41:01 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+/**
+ *main - Oututs the size of diff var types
+ *
+ *Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char c;
+	int d;
+	long int e;
+	long long int f;
+	float g;
+
+	printf("Size of a char: %dbyte(s)\n", sizeof(a));
+	printf("Size of a int: %dbyte(s)\n", sizeof(d));
+	printf("Size of a long int: %dbyte(s)\n", sizeof(e));
+	printf("Size of a long long int: %dbyte(s)\n", sizeof(f));
+	printf("Size of a float: %dbyte(s)\n", sizeof(g));
+	return (0);
+}
