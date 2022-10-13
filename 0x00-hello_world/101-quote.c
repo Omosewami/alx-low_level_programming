a15e5f88a62b15b514469c459fcae6e69a642b50
Omosewami <raimothassan10@gmail.com>
2022-10-13 16:52:53 -0700

    mmmm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- *main - out puts a text 
+ *main - out puts a text
  *Return: Always 0 (Successful)
  */
-int main (void)
+int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}	
+}
