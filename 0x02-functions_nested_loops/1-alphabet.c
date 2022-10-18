d7a750ef6c630f1335c6b77d7d927a35e78a2d9c
Omosewami <raimothassan10@gmail.com>
2022-10-18 16:34:17 -0700

    lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - lower case alphabet
+ */
+void print_alphabet(void)
+{
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+		_putchar(abc);
+
+	_putchar('\n');
+}
