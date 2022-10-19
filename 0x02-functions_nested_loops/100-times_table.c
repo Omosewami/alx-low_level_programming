d8c5d98cf8052e535f2d43031294e5f413ce1b78
Omosewami <raimothassan10@gmail.com>
2022-10-19 05:34:49 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - Prints the times table of the input,
- * starting with 0.
+ *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
@@ -19,9 +19,8 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-
 				prod = num * mult;
-				
+
 				if (prod <= 99)
 					_putchar(' ');
 				if (prod <= 9)
@@ -30,7 +29,8 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');				   }
+					_putchar(((prod / 10)) % 10 + '0');
+				}
 				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
@@ -40,4 +40,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-}	
+}
