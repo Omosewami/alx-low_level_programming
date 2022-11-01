298504da9ce5067752b9c78cf09ead7612269d8b
Omosewami <raimothassan10@gmail.com>
2022-10-31 19:01:16 -0700

    me

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  * @n: The number of bytes to copy from @src.
  * Return: A pointer to the destination buffer @dest.
  */
-voi *_memcpy(char *dest,char *src, unsigned int n)
+char *_memcpy(char *dest,char *src, unsigned int n)
 {
 
 	unsigned int index;
