47fd86a8fa69714ad9155a2dc8471f52c48e6d87
Omosewami <raimothassan10@gmail.com>
2022-10-31 20:01:08 -0700

    file strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,10 +16,10 @@ unsigned int _strspn(char *s, char *accept)
 		for (index = 0; accept[index]; index++)
 		{
 			if (*s == accept[index]
-			{
+			}
 				bytes++;
 				break;
-			}
+			{
 
 			else if (accept[index + 1] == '\0')
 				return (bytes);
