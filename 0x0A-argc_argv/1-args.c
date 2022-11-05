6461afbc8b81bcadaa2c765acf81e645931e05f7
Omosewami <raimothassan10@gmail.com>
2022-11-04 19:28:21 -0700

    all

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the number of arguments
+ * supplied to it
+ * @argc: argument count to be printed
+ * @argv: array containing pointers that points
+ * to the arguments.
+ *
+ *  Return: always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	if (argc >= 1)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
